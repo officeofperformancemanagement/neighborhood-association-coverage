7b7d47617723de6859fa0fe51e23997e82d9516c
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-11-19 15:06:20 -0500

    log geojson files

--- main.mjs
+++ main.mjs
@@ -4,6 +4,7 @@ import geoblaze from "geoblaze";
 // fetch boundaries of the City of Chattanooga
 const city_council_response = await fetch("https://internal.chattadata.org/resource/5t2x-jnde.geojson");
 const city_council_data = await city_council_response.json();
+writeFileSync("./data/city-council-districts.geojson", JSON.stringify(city_council_data, undefined, 2));
 
 // initalize population grid
 const population_url = "https://s3.amazonaws.com/geotiff.io/gpw_v4_population_density_rev11_2020_30_sec.cog.tif";
@@ -18,6 +19,7 @@ console.log("total:", total);
 // fetch neighborhood association boundaries
 const neighborhood_association_boundaries_response = await fetch("https://www.chattadata.org/resource/dxzz-idjy.geojson");
 const neighborhood_association_boundaries_data = await neighborhood_association_boundaries_response.json();
+writeFileSync("./data/neighborhood-association-boundaries.geojson", JSON.stringify(neighborhood_association_boundaries_data, undefined, 2));
 
 // calculation population covered by neighborhood associations
 // virtually resamples each pixel into 10,000 smaller sub pixels
