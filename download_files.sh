26bf0fc02355bb394d88851c9ba2ab054bc6120f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2025-01-20 08:28:58 -0500

    Update download_files.sh

--- download_files.sh
+++ download_files.sh
@@ -5,3 +5,6 @@ curl https://www.chattadata.org/resource/dxzz-idjy.geojson > ./data/neighborhood
 
 # download city council districts (which is used as city boundaries)
 curl https://internal.chattadata.org/resource/5t2x-jnde.geojson > ./data/city-council-districts.geojson
+
+# wait for cache to warm up
+sleep 5
